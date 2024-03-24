7b01a22a89a9bec1fb052b9c9119728f2528a291
aparnamule1401 <143073592+AparnaMule1401@users.noreply.github.com>
2024-03-24 10:59:33 +0530

    com

---
+++ Landing/src/app/data/data.js
@@ -0,0 +1,539 @@
+import {FiBriefcase, FiKey, FiPhone, FiMail,FiMapPin, LuTrees ,FiShield, GoRocket, MdDirectionsCar  } from '../assets/icons/vander'
+
+export const counterData = [
+    {
+        title:'Properties Sell',
+        target: '1548'
+    },
+    {
+        title:'Award Gained',
+        target: '25'
+    },
+    {
+        title:'Years Experience',
+        target: '9'
+    },
+]
+export const featureData =[
+    {
+        title:'Evaluate Your EV',
+        description:'If the distribution of letters and words is random, the reader will not be distracted from making.',
+        icon: MdDirectionsCar
+    },
+    {
+        title:'Meeting with Agent',
+        description:'If the distribution of letters and words is random, the reader will not be distracted from making.',
+        icon: FiBriefcase 
+    },
+    {
+        title:'Close the Deal',
+        description:'If the distribution of letters and words is random, the reader will not be distracted from making.',
+        icon: FiKey
+    }
+]
+
+export const teamData = [
+    {
+        images:'/images/client/04.jpg',
+        name:'Jack John',
+        designation:'Designer',
+    },
+    {
+        images:'/images/client/05.jpg',
+        name:'Jack John',
+        designation:'Designer',
+    },
+    {
+        images:'/images/client/06.jpg',
+        name:'Jack John',
+        designation:'Designer',
+    },
+    {
+        images:'/images/client/07.jpg',
+        name:'Jack John',
+        designation:'Designer',
+    },
+]
+export const review = [
+    {
+        id: '1',
+        profile: "/images/client/01.jpg",
+        name: 'Calvin Carlo',
+        designation: "Manager",
+        description: "Hously made the processes so easy. Hously instantly increased the amount of interest and ultimately saved us over $10,000."
+    },
+    {
+        id: '2',
+        profile:"/images/client/02.jpg",
+        name: 'Christa Smith',
+        designation: "Manager",
+        description: 'I highly recommend Hously as the new way to sell your home "by owner". My home sold in 24 hours for the asking price. Best $400 you could spend to sell your home.'
+    }, {
+        id: '3',
+        profile: "/images/client/03.jpg",
+        name: 'Christa Smith',
+        designation: "Manager",
+        description: "My favorite part about selling my home myself was that we got to meet and get to know the people personally. This made it so much more enjoyable!"
+    }, {
+        id: '4',
+        profile: "/images/client/04.jpg",
+        name: 'Christa Smith',
+        designation: "Manager",
+        description: "Great experience all around! Easy to use and efficient."
+    }
+]
+export const properties = [
+    {
+        id: 1,
+        image: '/images/property/s1.jpeg',
+        name: 'AUDI',
+        Model: "E-Tron",
+        Type: 'Automatic',
+        Range: '436km',
+        price: 400000,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    },
+    {
+        id: 2,
+        image: '/images/property/b1.jpeg',
+        name: 'Scott Bike',
+        Model: 'Solace',
+        Type: 'Manual',
+        Range: '70km',
+        price: 47999,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    },
+    {
+        id: 3,
+        image:'/images/property/s3.jpg',
+        name: 'li05-moped',
+        Model: 'EM050',
+        Type: 'Automatic',
+        Range: '100km',
+        price: 50000,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    },
+    {
+        id: 4,
+        image: '/images/property/s2.jpeg',
+        name: 'BMW iX',
+        Model:'E-Tron',
+        Type: 'Automatic',
+        Range: '436km',
+        price: 500000,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    },
+    {
+        id: 5,
+        image: '/images/property/b2.jpeg',
+        name: 'Scott Strike',
+        Model: 'Strike',
+        Type: 'Manual',
+        Range: '80km',
+        price: 19999,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    },
+    {
+        id: 6,
+        image: '/images/property/s4.jpeg',
+        name: 'Scooter',
+        Model: 'EM055',
+        Type: 'Automatic',
+        Range: '400km',
+        price: 45000,
+        detail: ['/images/property/single/s1.jpeg', '/images/property/single/2', '/images/property/single/3', '/images/property/single/4.jpg', '/images/property/single/5.jpg']
+    }
+];
+export const propertiesDetails = [
+    {
+        id: 1,
+        image: "/images/property/s11.jpg",
+        name: 'BMW',
+        Model: "X6",
+        Type: 'Automatic',
+        Range: '436km',
+        price: 400000,
+        button:'next',
+    },
+    {
+        id: 2,
+        image: "/images/property/buysearch1.jpeg",
+        name: 'toyota',
+        Model: 'bz4x',
+        Type: 'Automatic',
+        Range: '500km',
+        price: 379999,
+        button:'next',
+    },
+    {
+        id: 3,
+        image: "/images/property/buysearch2.jpeg",
+        name: 'honda',
+        Model: ' NS1',
+        Type: 'Automatic',
+        Range: '100km',
+        price: 50000,
+        button:'next',
+        
+    },
+    {
+        id: 4,
+        image: "/images/property/buysearch3.jpeg",
+        name: 'Nissan',
+        Model: 'JUKE',
+        Type: 'Automatic',
+        Range: '200km',
+        price: 47000,
+        button:'next',
+    },
+    {
+        id: 5,
+        image: "/images/property/buysearch4.jpeg",
+        name: 'Honda',
+        Model: 'HR',
+        Type: 'Automatic',
+        Range: '300km',
+        price: 50000,
+        button:'next'
+    },
+    {
+        id: 6,
+        image: '/images/property/s2.jpeg',
+        name: 'BMW iX',
+        Model:'E-Tron',
+        Type: 'Automatic',
+        Range: '436km',
+        price: 500000,
+        button:'next'
+    }
+];
+export const BikeDetails = [
+    {
+        id: 2,
+        image: '/images/property/b1.jpeg',
+        name: 'Scott Bike',
+        Model: 'Solace',
+        Type: 'Manual',
+        Range: '70km',
+        price: 47999,
+        button:'next',
+    },
+    {
+        id: 5,
+        image: '/images/property/b2.jpeg',
+        name: 'Scott Strike',
+        Model: 'Strike',
+        Type: 'Manual',
+        Range: '80km',
+        price: 19999,
+        button:'next',
+    },
+    {
+        id: 3,
+        image: "/images/property/p1.jpeg",
+        name: 'triumph-e',
+        Model: 'e ',
+        Type: 'Manual',
+        Range: '70km',
+        price: 40000,
+        button:'next',
+        
+    },
+    {
+        id: 4,
+        image: "/images/property/p2.jpeg",
+        name: 'motovolt',
+        Model: 'JU',
+        Type: 'Manual',
+        Range: '90km',
+        price: 47000,
+        button:'next',
+    },
+    {
+        id: 5,
+        image: "/images/property/p3.jpeg",
+        name: 'Honda',
+        Model: 'HR',
+        Type: 'Manual',
+        Range: '60km',
+        price: 30000,
+        button:'next'
+    },
+    {
+        id: 6,
+        image: '/images/property/p4.jpeg',
+        name: 'Kona',
+        Model:'E-Tron',
+        Type: 'Manual',
+        Range: '36km',
+        price: 500000,
+        button:'next'
+    }
+];
+export const scooterDetails = [
+    {
+        id: 2,
+        image: '/images/property/p5.jpeg',
+        name: 'Hero',
+        Model: 'x',
+        Type: 'Automatic',
+        Range: '100km',
+        price: 47999,
+        
+    },
+    {
+        id: 5,
+        image: '/images/property/s4.jpeg',
+        name: 'Scooter',
+        Model: 'EM055',
+        Type: 'Automatic',
+        Range: '400km',
+        price: 45000,
+        
+    },
+    {
+        image:'/images/property/s3.jpg',
+        name: 'li05-moped',
+        Model: 'EM050',
+        Type: 'Automatic',
+        Range: '100km',
+        price: 50000,
+        
+        
+    },
+    {
+        id: 4,
+        image: "/images/property/p6.jpeg",
+        name: 'ola',
+        Model: 'JUKE',
+        Type: 'Automatic',
+        Range: '200km',
+        price: 47000,
+        button:'next',
+    },
+    {
+        id: 5,
+        image: "/images/property/p7.jpeg",
+        name: 'Honda',
+        Model: 'HR',
+        Type: 'Automatic',
+        Range: '300km',
+        price: 50000,
+        button:'next'
+    },
+    {
+        id: 6,
+        image: '/images/property/p8.jpeg',
+        name: 'ola',
+        Model:'E-Tron',
+        Type: 'Automatic',
+        Range: '436km',
+        price: 500000,
+        button:'next'
+    }
+];
+export const blogList = [
+    {
+        id:1,
+        title: "Skills That You Can Learn In The Real Estate Market",
+        date: "13th Sep, 2023",
+        type: 'Industrial',
+        image: "/images/property/1.jpg"
+    },
+    {
+        id:2,
+        title: "Learn The Truth About Real Estate Industry",
+        date: "29th Nov, 2023",
+        type: 'Industrial',
+        image: "/images/property/2.jpg"
+    },
+    {
+        id:3,
+        title: "10 Quick Tips About Business Development",
+        date: "29th Dec, 2023",
+        type: 'Industrial',
+        image: "/images/property/3.jpg"
+    },
+    {
+        id:4,
+        title: "14 Common Misconceptions About Business Development",
+        date: "13th March, 2023",
+        type: 'Industrial',
+        image:"/images/property/4.jpg"
+    },
+    {
+        id:5,
+        title: "10 Things Your Competitors Can Teach You About Real Estate",
+        date: "5th May, 2023",
+        type: 'Industrial',
+        image: "/images/property/5.jpg"
+    },
+    {
+        id:6,
+        title: "Why We Love Real Estate",
+        date: "19th June, 2023",
+        type: 'Industrial',
+        image: "/images/property/6.jpg"
+    },
+    {
+        id:7,
+        title: "Skills That You Can Learn In The Real Estate Market",
+        date: "20th June, 2023",
+        type: 'Industrial',
+        image: "/images/property/6.jpg"
+    },
+    {
+        id:8,
+        title: "Learn The Truth About Real Estate Industry",
+        date: "31st Aug, 2023",
+        type: 'Industrial',
+        image: "/images/property/5.jpg"
+    },
+    {
+        id:9,
+        title: "10 Quick Tips About Business Development",
+        date: "1st Sep, 2023",
+        type: 'Industrial',
+        image: "/images/property/4.jpg"
+    },
+    {
+        id:10,
+        title: "14 Common Misconceptions About Business Development",
+        date: "3rd March, 2023",
+        type: 'Industrial',
+        image:"/images/property/3.jpg"
+    },
+    {
+        id:11,
+        title: "10 Things Your Competitors Can Teach You About Real Estate",
+        date: "3rd March, 2023",
+        type: 'Industrial',
+        image: "/images/property/2.jpg"
+    },
+    {
+        id:12,
+        title: "Why We Love Real Estate",
+        date: "3rd March, 2023",
+        type: 'Industrial',
+        image: "/images/property/1.jpg"
+    }
+]
+export const conatctDetails = [
+    {
+        title:'Phone',
+        description:'The phrasal sequence of the is now so that many campaign and benefit',
+        contact:'+152 534-468-854',
+        Icon:FiPhone
+    },
+    {
+        title:'Email',
+        description:'The phrasal sequence of the is now so that many campaign and benefit',
+        contact:'contact@example.com',
+        Icon:FiMail
+    },
+    {
+        title:'Location',
+        description:'The phrasal sequence of the is now so that many campaign and benefit',
+        contact:'C/54 Northwest Freeway, Suite 558,  Houston, USA 485',
+        Icon:FiMapPin
+    },
+]
+export const accordion = [
+    {
+        title: 'How does it work ?',
+        content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.'
+    },
+    {
+        title: 'Do I need a designer to use Hously ?',
+        content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.'
+    },
+    {
+        title: 'What do I need to do to start selling ?',
+        content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.'
+    },
+    {
+        title: 'What happens when I receive an order ?',
+        content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.'
+    }
+]
+export const feature = [
+    {
+        title:'Comfortable',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-cards-heart',
+    },
+    {
+        title:'Extra Security',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-shield-sun',
+    },
+    {
+        title:'Luxury',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-star',
+    },
+    {
+        title:'Best Price',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-currency-usd',
+    },
+    {
+        title:'Stratagic Location',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-map-marker',
+    },
+    {
+        title:'Efficient',
+        description:"If the distribution of letters and words is random, the reader will not be distracted from making.",
+        icon:'mdi mdi-chart-arc',
+    }
+]
+export const listingData = [
+    {
+        image:"/images/property/residential.jpg",
+        title:'Residential',
+        noOfListing:46
+    },
+    {
+        image:"/images/property/land.jpg",
+        title:'Land',
+        noOfListing:124
+    },
+    {
+        image:"/images/property/commercial.jpg",
+        title:'Commercial',
+        noOfListing:265
+    },
+    {
+        image:"/images/property/investment.jpg",
+        title:'Industrial',
+        noOfListing:452
+    },
+    {
+        image:"/images/property/industrial.jpg",
+        title:'Investment',
+        noOfListing:12
+    },
+]
+export const pricingPlan = [
+    {
+        title:'Basic',
+        time:19,
+        Icon:LuTrees ,
+        fearures:['Full Access','Source Files','Free Appointments','Enhanced Security']
+    },
+    {
+        title:'Premium',
+        time:39,
+        Icon:FiShield ,
+        fearures:['Full Access','Source Files','Free Appointments','Enhanced Security']
+    },
+    {
+        title:'Business',
+        time:99,
+        Icon:GoRocket,
+        fearures:['Full Access','Source Files','Free Appointments','Enhanced Security']
+    },
+
+]
+
